4d872cc7c0db9b4b0049cea573f6604e7740a4bf
Ramandeepkaur53 <157076187+Ramandeepkaur53@users.noreply.github.com>
2024-02-19 18:42:33 +0300

    Update MainActivity.java
    updated mainActivity.java

--- Ramandeep-AndroidLabs/app/src/main/java/com/example/ramandeepkaursandroidlabs/MainActivity.java
+++ Ramandeep-AndroidLabs/app/src/main/java/com/example/ramandeepkaursandroidlabs/MainActivity.java
@@ -2,30 +2,65 @@ package com.example.ramandeepkaursandroidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginButton;
+    EditText emailEditText;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView mytext = findViewById(R.id.myTextViewId);
-        Button button = findViewById(R.id.myButton);
-        EditText myedit = findViewById(R.id.myedittext);
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+        loginButton = findViewById(R.id.loginButton);
+        emailEditText = findViewById(R.id.edit1);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String editString = myedit.getText().toString();
-                mytext.setText( "Your edit text has: " + editString);
-            }
-        });
+        loginButton.setOnClickListener( clk-> {
+
+            Intent nextPage = new Intent( MainActivity.this, SecondActivity.class);
+            nextPage.putExtra( "EmailAddress", emailEditText.getText().toString() );
+            startActivity(nextPage);
+        } );
 
 
     }
+}
 
 }
